5712c1130956c6077d1cf690a64ea255f7083a77
Jaesung Ryu <cwryu6252@naver.com>
2023-06-09 14:34:02 +0900

    3.5

--- 3.5/create_s3_bucket_block.py
+++ 3.5/create_s3_bucket_block.py
@@ -1,10 +1,22 @@
+import json
 from time import sleep
 from prefect_aws import S3Bucket, AwsCredentials
 
 
+# prefecthq.github.io/prefect-aws/s3
+def load_credential():
+    # run in prefect-mlops-zoomcamp folder
+    with open("./3.5/creds.json", "r") as f:
+        creds = json.load(f)
+    return creds
+
+
 def create_aws_creds_block():
+    creds = load_credential()
+
     my_aws_creds_obj = AwsCredentials(
-        aws_access_key_id="123abc", aws_secret_access_key="abc123"
+        aws_access_key_id=creds["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=creds["AWS_SECRET_ACCESS_KEY"],
     )
     my_aws_creds_obj.save(name="my-aws-creds", overwrite=True)
 
@@ -12,9 +24,10 @@ def create_aws_creds_block():
 def create_s3_bucket_block():
     aws_creds = AwsCredentials.load("my-aws-creds")
     my_s3_bucket_obj = S3Bucket(
-        bucket_name="my-first-bucket-abc", credentials=aws_creds
+        bucket_name="kade-mlops-zoomcamp-2023",
+        credentials=aws_creds,
     )
-    my_s3_bucket_obj.save(name="s3-bucket-example", overwrite=True)
+    my_s3_bucket_obj.save(name="s3-bucket-block", overwrite=True)
 
 
 if __name__ == "__main__":
